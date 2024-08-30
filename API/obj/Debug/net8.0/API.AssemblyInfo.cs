b3d90fc0bcb7cb7e921c3043a2b206ca48174779
Jpdl925 <jtpg925@gmail.com>
2024-08-29 18:10:57 -0700

    Up To Date

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+272804e8bba22d703028d29d87029455178bfad9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18536ab4d78bc37578413bb732318ae9cd7e5238")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
